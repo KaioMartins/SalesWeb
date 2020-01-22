8731f91438c88fc599f7382021c5d59b2262797f
Kaio Martins <kaio.ccmartins@gmail.com>
2020-01-22 13:05:16 -0300

    Pasta 17 - "16. SellersController"

--- SalesWeb/Startup.cs
+++ SalesWeb/Startup.cs
@@ -3,15 +3,16 @@ using System.Threading.Tasks;
 using Microsoft.Owin;
 using Owin;
 
-[assembly: OwinStartup(typeof(SalesWeb.Startup1))]
+[assembly: OwinStartup(typeof(SalesWeb.Startup))]
 
 namespace SalesWeb
 {
-    public class Startup1
+    public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+
         }
     }
 }
